28816299810b7f07f0a4708e8cf5bf7f4bca4a0f
miniesta4 <mgnstg@gmail.com>
2015-03-06 20:17:13 +0100

    comments improved

--- plot2.R
+++ plot2.R
@@ -1,21 +1,21 @@
 plot2 <- function(){
-  # vars classes
+  ## File to read in working directory.
+  
+  ## Read header
+  header <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
+
+  ## Read dataset dates 1/2/2007, 2/2/2007
   classes <- c(rep("character", 2), rep("numeric", 7))
+  hpc <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, skip = 66637, nrows = 2880, stringsAsFactors = FALSE)
 
-  ## read header
-  header <- read.table("household_power_consumption.txt", header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
-
-  ## read dataset dates 1/2/2007, 2/2/2007
-  hpc <- read.table("household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, skip = 66637, nrows = 2880, stringsAsFactors = FALSE)
-
-  ## set vars names from header
+  ## Set vars names from header
   names(hpc) <- header
 
-  ## add date/time var
+  ## Add date/time var
   dt <- paste(hpc$Date, hpc$Time)
   hpc$datetime <- strptime(dt, "%d/%m/%Y %H:%M:%S")
   
-  ## devise plot
+  ## Devise plot
   png(file = "plot2.png")
   with(hpc, plot(datetime, Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
   dev.off()
--- plot4.R
+++ plot4.R
@@ -1,12 +1,12 @@
 plot4 <- function(){
-  # Vars classes
-  classes <- c(rep("character", 2), rep("numeric", 7))
-
+  ## File to read in working directory.
+  
   ## Read header
-  header <- read.table("household_power_consumption.txt", header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
+  header <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", nrows = 1, stringsAsFactors = FALSE)
 
   ## Read dataset dates 1/2/2007, 2/2/2007
-  hpc <- read.table("household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, skip = 66637, nrows = 2880, stringsAsFactors = FALSE)
+  classes <- c(rep("character", 2), rep("numeric", 7))
+  hpc <- read.table("./household_power_consumption.txt", header = FALSE, sep = ";", colClasses = classes, skip = 66637, nrows = 2880, stringsAsFactors = FALSE)
 
   ## Set vars names from header
   names(hpc) <- header
